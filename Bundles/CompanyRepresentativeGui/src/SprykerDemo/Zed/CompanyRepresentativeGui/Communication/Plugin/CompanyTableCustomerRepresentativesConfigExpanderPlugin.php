c43295480c7dfddfce13dcf680602d488d3a70e5
mabdullah <mohamed.abdullah@spryker.com>
2023-04-10 11:02:33 +0200

    Introduced table config

---
+++ Bundles/CompanyRepresentativeGui/src/SprykerDemo/Zed/CompanyRepresentativeGui/Communication/Plugin/CompanyTableCustomerRepresentativesConfigExpanderPlugin.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerDemo\Zed\CompanyRepresentativeGui\Communication\Plugin;
+
+use Generated\Shared\Transfer\CustomerRepresentativesFilterTransfer;
+use Orm\Zed\Company\Persistence\Map\SpyCompanyTableMap;
+use Spryker\Zed\CompanyGuiExtension\Dependency\Plugin\CompanyTableConfigExpanderPluginInterface;
+use Spryker\Zed\Gui\Communication\Table\TableConfiguration;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+
+/**
+ * @method \SprykerDemo\Zed\CompanyRepresentativeGui\Communication\CompanyRepresentativeGuiCommunicationFactory getFactory()
+ */
+class CompanyTableCustomerRepresentativesConfigExpanderPlugin extends AbstractPlugin implements CompanyTableConfigExpanderPluginInterface
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @param \Spryker\Zed\Gui\Communication\Table\TableConfiguration $config
+     *
+     * @return \Spryker\Zed\Gui\Communication\Table\TableConfiguration
+     */
+    public function expandConfig(TableConfiguration $config): TableConfiguration
+    {
+        return $config;
+    }
+}
